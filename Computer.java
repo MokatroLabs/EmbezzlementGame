df4c1fc74740a6dc71ff5f282d391bb9ab57a31a
DarkX301 <39450650+DarkX301@users.noreply.github.com>
2018-05-19 21:13:12 -0700

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,29 @@
+
+public class Computer implements Players {
+
+	@Override
+	public double getReputation() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMoney() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getFaction() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public void activeAbility()
+	{
+		
+	}
+
+	
+
+}
