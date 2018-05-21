53a6dd582f1c4d6637b5a270c22ba7778368e3c6
Majestic-12 <javin@paryani.com>
2018-05-21 12:32:58 -0700

    sddfgh

--- Computer.java
+++ Computer.java
@@ -2,7 +2,7 @@
 public class Computer implements Players {
 
 	@Override
-	static bankAccount = 100;
+	
 	public double getReputation() {
 		// TODO Auto-generated method stub
 		return 0;
@@ -26,14 +26,10 @@ public class Computer implements Players {
 	}
 	public void fundraise()
 	{
-		bandFund += 50;
-		reputation += 3;
+		
 	}
 	public void embezzle()
-	{   //takes money from abnd fund and puts it in bank account
-		int stealcash = (int) math.random() * 50;
-		band Fund -= stealCash;
-		bankAccount += stealCash;
+	{   
 		
 		
 				
