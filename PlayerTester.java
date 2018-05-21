ed08f184001b8807d6c0f30c41d02e4d1cbd7466
DarkX301 <39450650+DarkX301@users.noreply.github.com>
2018-05-21 16:00:22 -0700

    Update PlayerTester.java

--- PlayerTester.java
+++ PlayerTester.java
@@ -3,18 +3,12 @@ public class PlayerTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Human player = new Human();
-		System.out.println(player.getMoney());
-		System.out.println(player.getReputation());
-		player.raiseFunds();
-		System.out.println(player.getMoney());
-		System.out.println(player.getReputation());
 		
 		HQueen queen = new HQueen();
 		System.out.println(queen.getFaction());
 		System.out.println(queen.getMoney());
 		System.out.println(queen.getReputation());
-		queen.raiseFunds();
+		queen.fundRaise();
 		System.out.println(queen.getMoney());
 		queen.activeAbility();
 		System.out.println(queen.getMoney());
