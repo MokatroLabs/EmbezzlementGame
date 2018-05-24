5935e57ded544dc5369e4c596dab1469cbc16897
DarkX301 <39450650+DarkX301@users.noreply.github.com>
2018-05-24 08:52:22 -0700

    My Version V2

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
 
 public class Main {
 
+	final static int maxTurns = 150;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Mechanics economy = new Mechanics();
+		HQueen play = new HQueen();
+		while(economy.getTurns() <= maxTurns) {
+			if(play.getChar().equals("Business Man")) {
+				play.setMoney(play.getMoney() + 150);
+			}
+			
+			
+			economy.setTurns(economy.getTurns() + 1);
+		}
+		
 	}
 
 }
