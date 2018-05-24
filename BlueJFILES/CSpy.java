aef862829e6fc585b75a0267337644ae299686ee
Majestic-12 <javin@paryani.com>
2018-05-24 09:20:36 -0700

    donso

--- BlueJFILES/CSpy.java
+++ BlueJFILES/CSpy.java
@@ -9,27 +9,10 @@
  */
 public class CSpy extends Computer
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class CSpy
-     */
-    public CSpy()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+	public void embezzle()
+	{
+		money += 300;
+		reputation -= 0.7;
+		
+	}
 }
