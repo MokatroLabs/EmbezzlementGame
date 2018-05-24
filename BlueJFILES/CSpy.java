96550c5d46a2fa3d9a404fd2e52a08b4dba4b78b
Logan Pierce <39424414+Mokatro@users.noreply.github.com>
2018-05-24 10:22:37 -0700

    New Main and maybe fix spy

--- BlueJFILES/CSpy.java
+++ BlueJFILES/CSpy.java
@@ -9,44 +9,10 @@
  */
 public class CSpy extends Computer
 {
-	public CSpy
-	{
-		super();
-		this.character = "Spy"
-		
-	}
 	public void embezzle()
 	{
-		
-		int moneySteal = (int)math.random() * 300;
-		money += moneySteal;
-		reputation -= 1.7;
-		
+		money += 300;
+		reputation -= 0.7;
 		
 	}
-	public void getMoney()
-	{
-		return money;
-	}
-	public void activeAbility(PLayer target)
-	{
-		target.setReputation(player.getReputation - Math.random() * 20 )
-	}
-			
-	
-	public void fundraise()
-	{
-		System.out.print("can't do that");
-	}
-	public String getFaction()
-	{
-		
-		return("Spy");
-	}
-	public void setReputation(double newRep)
-	{
-		Reputation = newRep;
-	}
-	
-	
 }
--- BlueJFILES/Main.java
+++ BlueJFILES/Main.java
@@ -8,13 +8,27 @@
  * @version (a version number or a date)
  */
 import java.util.ArrayList;
-public class Main
-{
-    ArrayList<Player> players = new ArrayList<Player>();
-    public static void main (String[] args)
-    {
+public class Main {
+
+    final static int maxTurns = 150;
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
         Board board = new Board();
-        Mechanics mechanics = new Mechanics(400);
+        Mechanics economy = new Mechanics();
+        HQueen human = new HQueen();
+        CSpy  comp = new CSpy();
+        ArrayList<Player> players = new ArrayList<Player>();
+        int currentPlayer = 0;
+        while(economy.getTurns() <= maxTurns) {
+            if (currentPlayer  < players.size() )
+                currentPlayer++;
+            else
+                currentPlayer = 1;
+            
+            
+            economy.setTurns(economy.getTurns() + 1);
+        }
         
     }
+
 }
