032254a74029743ddb86e73470e1d7be6a272f96
Majestic-12 <javin@paryani.com>
2018-05-24 09:30:13 -0700

    asdfg

--- BlueJFILES/CSpy.java
+++ BlueJFILES/CSpy.java
@@ -21,10 +21,29 @@ public class CSpy extends Computer
 	}
 	public void activeAbility(int player)
 	{
-		if (player < 1 || player > 4)
+		if (player < 2 || player > 4)
 		{
 			activeAbility(int player);
 		}
-		else if()
+		else if(player == 2)
+		{
+			
+			
+		}
+		else if(player == 3)
+		{
+			
+			
+		}
+		else
+		{
+			
+			
+		}
+	}
+	public void fundraise()
+	{
+		System.out.print("can't do that");
+	}
 	}
 }
