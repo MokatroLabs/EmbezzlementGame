629722404031f56982d2c2e71adad2c68c2cf8bb
Majestic-12 <javin@paryani.com>
2018-05-24 09:26:57 -0700

    addsstuff

--- BlueJFILES/CSpy.java
+++ BlueJFILES/CSpy.java
@@ -11,8 +11,20 @@ public class CSpy extends Computer
 {
 	public void embezzle()
 	{
-		money += 300;
-		reputation -= 0.7;
+		setMoney(money += 300);
+		setReputation(reputation -= 0.7);
 		
+	}
+	public void getMoney()
+	{
+		return money;
+	}
+	public void activeAbility(int player)
+	{
+		if (player < 1 || player > 4)
+		{
+			activeAbility(int player);
+		}
+		else if()
 	}
 }
